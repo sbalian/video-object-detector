a7b7a0db5be63ab788050732d99d553e58b2db15
Seto Balian <seto.balian@gmail.com>
2024-01-28 17:35:44 +0000

    Update CLI

--- bicycle_detector/cli.py
+++ bicycle_detector/cli.py
@@ -1,15 +1,18 @@
+import concurrent.futures
+import fractions
+import functools
 import pathlib
 from typing import Annotated
 
 import typer
 
-from . import classifier
+from . import video
 
 app = typer.Typer()
 
 
 @app.command()
-def main(
+def frames(
     input: Annotated[
         pathlib.Path,
         typer.Argument(
@@ -17,17 +20,44 @@ def main(
             file_okay=False,
             dir_okay=True,
             resolve_path=True,
+            help="Directory to search for .dav files in to extract from.",
         ),
     ],
     output: Annotated[
         pathlib.Path,
-        typer.Argument(
+        typer.Option(
             file_okay=False,
             dir_okay=True,
             resolve_path=True,
+            help="Output directory.",
         ),
-    ],
+    ] = pathlib.Path("."),
+    workers: Annotated[
+        int,
+        typer.Option(
+            help="Number of maximum workers (0 for number of available CPUs).",
+        ),
+    ] = 0,
 ):
+    """Extract frames from .dav videos in INPUT to JPEGs on disk."""
+
+    output.mkdir(exist_ok=True)
     paths = sorted(list(input.glob("*.dav")))
-    for path in paths:
-        classifier.run_for_video(path, output)
+
+    extract = functools.partial(
+        video.extract_frames, fps=fractions.Fraction(1, 3), output_root=output
+    )
+
+    if workers == 0:
+        max_workers = None
+    else:
+        max_workers = workers
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers) as executor:
+        list(executor.map(extract, paths))
+
+
+@app.command()
+def classify():
+    """Run bicycle classification on JPEGs in INPUT directory."""
+    pass
