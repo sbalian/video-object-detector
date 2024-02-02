877b81a3c0334dc378a831b5009dba65c80809d6
Seto Balian <seto.balian@gmail.com>
2024-02-02 23:34:42 +0000

    Improve frame extractor

--- object_detector/video.py
+++ object_detector/video.py
@@ -25,14 +25,25 @@ def is_video(path: pathlib.Path) -> bool:
 def extract_frames(
     video_path: pathlib.Path,
     fps: fractions.Fraction,
-    output_directory: pathlib.Path,
+    ffmpeg_output: str = "%04d.jpeg",
 ) -> None:
+    """Extract frames from `video_path`.
+
+    Frames are written to a new directory `video_path`.frames.
+
+    `fps` sets the frames per second.
+
+    `ffmpeg_output` defaults to '%04d.jpeg'.
+    """
+
+    output_directory = video_path.parent / f"{video_path.stem}.frames"
+
     if output_directory.exists() and output_directory.is_file():
         raise NotADirectoryError(f"{output_directory}")
-    output_directory.mkdir(exist_ok=True)
-    path = output_directory / "%04d.jpeg"
-    str_fps = f"{fps.numerator}/{fps.denominator}"
-    ffmpeg.input(video_path).filter("fps", fps=str_fps).output(
-        str(path), loglevel="quiet"
+    output_directory.mkdir(exist_ok=False)
+
+    ffmpeg.input(video_path).filter(
+        "fps", fps=f"{fps.numerator}/{fps.denominator}"
+    ).output(
+        (output_directory / ffmpeg_output).as_posix(), loglevel="quiet"
     ).run()
-    return
