1c7210dc55724b166137b0dd6172e1eb86e330b9
Seto Balian <seto.balian@gmail.com>
2024-02-02 23:35:00 +0000

    Add tests for frame extractor

--- tests/test_video.py
+++ tests/test_video.py
@@ -1,3 +1,5 @@
+import fractions
+import os
 import pathlib
 
 import ffmpeg
@@ -31,3 +33,23 @@ def test_is_video_returns_false_for_wav_file(sample_wav):
 
 def test_is_video_returns_true_for_video_file(sample_video):
     assert video.is_video(sample_video)
+
+
+def test_extract_frames(sample_video):
+    video.extract_frames(sample_video, fractions.Fraction(5, 1))
+    assert sorted(
+        os.listdir(sample_video.parent / f"{sample_video.stem}.frames")
+    ) == [f"{i + 1}".zfill(4) + ".jpeg" for i in range(10)]
+
+
+def test_extract_frames_raises_for_existing_directory(sample_video):
+    (sample_video.parent / f"{sample_video.stem}.frames").mkdir()
+    with pytest.raises(FileExistsError):
+        video.extract_frames(sample_video, fractions.Fraction(5, 1))
+
+
+def test_extract_frames_raises_for_existing_file(sample_video):
+    with open(sample_video.parent / f"{sample_video.stem}.frames", "w") as f:
+        f.write("foo")
+    with pytest.raises(NotADirectoryError):
+        video.extract_frames(sample_video, fractions.Fraction(5, 1))
