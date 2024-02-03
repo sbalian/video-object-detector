b42fb442ac39e4e0c342ee88ddfdb3dd7e9a55a4
Seto Balian <seto.balian@gmail.com>
2024-02-03 00:28:21 +0000

    Improve classifier submodule

--- object_detector/classifier.py
+++ object_detector/classifier.py
@@ -1,5 +1,9 @@
+"""Detect objects in images using facebook/detr-resnet-101.
+
+Model from: https://huggingface.co/facebook/detr-resnet-101.
+"""
+
 import dataclasses
-import json
 import pathlib
 
 import torch
@@ -12,6 +16,21 @@ class Prediction:
     labels: list[str]
     scores: list[float]
 
+    def top_n(self, n: int, threshold: float) -> list[tuple[float, str]]:
+        """Return top `n` labels sorted by score > `threshold`."""
+
+        if not (0.0 <= threshold <= 1.0):
+            raise ValueError(f"threshold {threshold} out of bounds [0.0, 1.0]")
+        return sorted(
+            [
+                (score, label)
+                for score, label in zip(self.scores, self.labels)
+                if score > threshold
+            ],
+            reverse=True,
+            key=lambda x: x[0],
+        )[:n]
+
 
 @dataclasses.dataclass
 class Classifier:
@@ -19,16 +38,23 @@ class Classifier:
     model: transformers.DetrForObjectDetection = dataclasses.field(init=False)
 
     def __post_init__(self):
+        model_name = "facebook/detr-resnet-101"
+        revision = "no_timm"
         self.processor = transformers.DetrImageProcessor.from_pretrained(
-            "facebook/detr-resnet-101", revision="no_timm"
+            model_name, revision=revision
         )
         self.model = transformers.DetrForObjectDetection.from_pretrained(
-            "facebook/detr-resnet-101", revision="no_timm"
+            model_name, revision=revision
         )
         if torch.cuda.is_available():
             self.model.to("cuda")
 
     def predict(self, images: list[Image.Image]) -> list[Prediction]:
+        """Return predictions for a list of PIL images `images`.
+
+        Uses GPU if CUDA is available.
+        """
+
         inputs = self.processor(images=images, return_tensors="pt")
         if torch.cuda.is_available():
             inputs = inputs.to("cuda")
@@ -40,32 +66,28 @@ class Classifier:
         prob = torch.nn.functional.softmax(out_logits, -1)
         scores, labels = prob[..., :-1].max(-1)
 
-        results = []
-        for score, label in zip(scores, labels):
+        results: list[Prediction] = []
+        for scores_, labels_ in zip(scores, labels):
             prediction = Prediction(
-                labels=[self.model.config.id2label[ll.item()] for ll in label],
-                scores=[float(s) for s in score],
+                labels=[
+                    self.model.config.id2label[label.item()]
+                    for label in labels_
+                ],
+                scores=[float(score) for score in scores_],
             )
             results.append(prediction)
         return results
 
 
-def run_for_frames(frames_directory: pathlib.Path) -> None:
-    image_paths = list(frames_directory.glob("*.jpeg"))
+def batch_run(
+    image_paths: list[pathlib.Path], batch_size: int = 10
+) -> list[Prediction]:
 
     clf = Classifier()
-    batch_size = 10
+    predictions = []
 
-    results = {}
     for i in range(0, len(image_paths), batch_size):
         batch = image_paths[i : i + batch_size]
         images = [Image.open(image_path) for image_path in batch]
-        predictions = clf.predict(images)
-        for image_path, prediction in zip(batch, predictions):
-            results[str(image_path)] = {
-                "scores": prediction.scores,
-                "labels": prediction.labels,
-            }
-
-    with open(frames_directory / "pred.json", "w") as f:
-        json.dump(results, f)
+        predictions.extend(clf.predict(images))
+    return predictions
