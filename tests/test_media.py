d76c98fd6c2e5ecfe2245e28a3a19a3b7b9b5056
Seto Balian <seto.balian@gmail.com>
2024-02-04 21:52:09 +0000

    Rename video submodule and refactor tests

--- tests/test_media.py
+++ tests/test_media.py
@@ -2,9 +2,24 @@ import fractions
 import os
 import pathlib
 
+import moviepy.editor as mpy
+import numpy as np
 import pytest
 
-from object_detector import video
+from object_detector import media
+
+
+def make_random_image(time) -> np.ndarray:
+    imarray = np.random.rand(100, 100, 3) * 255
+    return imarray.astype("uint8")
+
+
+@pytest.fixture
+def sample_video(tmp_path):
+    path = tmp_path / "foo.mp4"
+    clip = mpy.VideoClip(make_random_image, duration=5)
+    clip.write_videofile(path.as_posix(), fps=25)
+    return path
 
 
 @pytest.mark.parametrize(
@@ -23,11 +38,11 @@ from object_detector import video
     ],
 )
 def test_is_likely_video(test_input, expected):
-    assert video.is_likely_video(test_input) == expected
+    assert media.is_likely_video(test_input) == expected
 
 
 def test_extract_frames(sample_video):
-    video.extract_frames(sample_video, fractions.Fraction(5, 1))
+    media.extract_frames(sample_video, fractions.Fraction(5, 1))
     assert sorted(
         os.listdir(sample_video.parent / f"{sample_video.stem}.frames")
     ) == [f"{i + 1}".zfill(4) + ".jpeg" for i in range(25)]
@@ -37,5 +52,5 @@ def test_extract_frames_raises_for_text_file(tmp_path):
     path = tmp_path / "file.txt"
     with open(path, "w") as f:
         f.write("foo")
-    with pytest.raises(video.FrameExtractionError):
-        video.extract_frames(path, fractions.Fraction(5, 1))
+    with pytest.raises(media.FrameExtractionError):
+        media.extract_frames(path, fractions.Fraction(5, 1))
