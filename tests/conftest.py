87c1c3625fd00def0cfff3df95bf41f8b9a27c6c
Seto Balian <seto.balian@gmail.com>
2024-02-04 16:04:04 +0000

    Update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 import moviepy.editor as mpy
 import numpy as np
 import pytest
-import wavio
 
 
 def make_random_image(time) -> np.ndarray:
@@ -14,12 +13,4 @@ def sample_video(tmp_path):
     path = tmp_path / "foo.mp4"
     clip = mpy.VideoClip(make_random_image, duration=5)
     clip.write_videofile(path.as_posix(), fps=25)
-    return path
-
-
-@pytest.fixture
-def sample_wav(tmp_path):
-    path = tmp_path / "foo.wav"
-    rate = 44100
-    wavio.write(path.as_posix(), np.ones(rate), rate, sampwidth=2)
     return path
